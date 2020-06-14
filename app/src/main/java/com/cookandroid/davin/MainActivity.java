4c56bc7f03b77dd9a0df805b52e6dddcbd451536
yuyw0712 <yuyw0712@naver.com>
2020-06-14 21:35:19 +0900

    브금까지완료

--- app/src/main/java/com/cookandroid/davin/MainActivity.java
+++ app/src/main/java/com/cookandroid/davin/MainActivity.java
@@ -1,15 +1,24 @@
 package com.cookandroid.davin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.SoundPool;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
      TextView start,tuto,exit;
+     Button startbtn;
+    MediaPlayer mediaPlayer ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,7 +26,55 @@ public class MainActivity extends AppCompatActivity {
         start=findViewById(R.id.start);
         tuto=findViewById(R.id.tuto);
         exit=findViewById(R.id.exit);
+        startbtn=findViewById(R.id.startbtn);
+        mediaPlayer= MediaPlayer.create(this,R.raw.main_bgm);
+      hidenavigate();
+
+
     }
+public void hidenavigate()
+{
+    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    View decor_View = getWindow().getDecorView();
+    int ui_Options = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+    decor_View.setSystemUiVisibility(ui_Options);
+}
+
+    public void startbtnclick(View v)
+    {
+
+      if(mediaPlayer.isPlaying()==true){
+          startbtn.setBackgroundResource(R.drawable.umx);
+          mediaPlayer.pause();
+      }
+      else
+      {
+          startbtn.setBackgroundResource(R.drawable.umo);
+          mediaPlayer.start();
+      }
+    }
+public void onUserLeaveHint(){
+        mediaPlayer.pause();
+        super.onUserLeaveHint();
+}
+public void onResume(){
+        mediaPlayer.start();
+        super.onResume();
+}
+public void onDestroy(){
+        mediaPlayer.stop();
+        super.onDestroy();
+}
+public void onBackPressed(){
+        mediaPlayer.stop();
+        super.onBackPressed();
+}
    public void startClick(View v)
     {
         Intent startintent = new Intent(getApplicationContext(),StartActivity.class);
@@ -25,10 +82,14 @@ public class MainActivity extends AppCompatActivity {
     }
     public void tutoClick(View v)
     {
-
+     Intent tutointent = new Intent(getApplicationContext(),tuto_1.class);
+     startActivity(tutointent);
     }
     public void exitClick(View v)
     {
-        finish();
+
+        ActivityCompat.finishAffinity(MainActivity.this);
+        System.exit(0);
+
     }
 }
--- app/src/main/java/com/cookandroid/davin/StartActivity.java
+++ app/src/main/java/com/cookandroid/davin/StartActivity.java
@@ -1,15 +1,70 @@
 package com.cookandroid.davin;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StartActivity extends AppCompatActivity  {
+    MediaPlayer startplayer ;
+    Button startbtn;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_activity);
+        startbtn=findViewById(R.id.startbtn);
+        startplayer= MediaPlayer.create(this,R.raw.game_bgm);
+       hidenavigate();
 
+    }
+    /*
+    ** 네비게이션 숨기기
+     */
+    public void hidenavigate()
+    {
+        View decor_View = getWindow().getDecorView();
+        int ui_Options = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        decor_View.setSystemUiVisibility(ui_Options);
 
+    }
+    /*
+    ** 사운드 넣기.
+     */
+    public void startbtnclick(View v)
+    {
+
+        if(startplayer.isPlaying()==true){
+            startbtn.setBackgroundResource(R.drawable.umx);
+            startplayer.pause();
+        }
+        else
+        {
+            startbtn.setBackgroundResource(R.drawable.umo);
+            startplayer.start();
+        }
+    }
+    public void onUserLeaveHint(){
+        startplayer.pause();
+        super.onUserLeaveHint();
+    }
+
+    public void onDestroy(){
+        startplayer.stop();
+        super.onDestroy();
+    }
+    public void onBackPressed(){
+        startplayer.stop();
+        super.onBackPressed();
+    }
+    public void onResume(){
+        startplayer.start();
+        super.onResume();
     }
 }
